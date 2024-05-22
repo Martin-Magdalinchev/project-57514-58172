0966d2e5a1e043b40518560bbecdc40754a150ff
Martin <m.magdalinchev@campus.fct.unl.pt>
2024-05-22 01:13:07 +0100

    project setup

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/pmtbl/Desktop/CP/project-57514-58172
+# Build directory: /mnt/c/Users/pmtbl/Desktop/CP/project-57514-58172/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("libwb")
+subdirs("googletest/build")
+subdirs("test")
---
+++ build/googletest/build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/pmtbl/Desktop/CP/project-57514-58172/build/googletest/src/googlemock
+# Build directory: /mnt/c/Users/pmtbl/Desktop/CP/project-57514-58172/build/googletest/build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
