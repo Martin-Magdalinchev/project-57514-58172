c8092360f66430e47f05667394067a20998f05c3
Martin <m.magdalinchev@campus.fct.unl.pt>
2024-06-03 10:13:46 +0100

    Report almost ccompleted

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/martin/Documents/CP/project-57514-58172
-# Build directory: /home/martin/Documents/CP/project-57514-58172/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libwb")
-subdirs("googletest/build")
-subdirs("test")
--- build/test/CMakeFiles/histogram_eq_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for histogram_eq_test.
-# This may be replaced when dependencies are built.
